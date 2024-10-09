3f9531c963c4d1f6de51bf8fcc769d5aa585814b
Sivakumar1001 <sivakumarkrishnan1001@gmail.com>
2024-10-09 16:44:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <center>
+            <h1><b>SIVAKUMAR  K</b></h1>
+        </center>
+        <h5 class="cnt">CONTACT DETAILS</h5>
+        <ul>
+            <li> Phone no:6383813053</li>
+            <li>Email id:sivakumarkrishnan1001@gmail.com</li>
+            <li>Address:chennai,tamilnadu</li>
+            <li>github:https://github.com/Sivakumar1001</li>
+            <li>https://wwww.linkedin.com/in/sivakumar1001</li>
+        </ul>
+        <h5 class="cnt">ABOUT ME</h5>
+            <p> i am beginner in graphic design,currently learning the basics of figma for UI/UX and flutter for app development.i am eager to apply my skills in designing real-world projects.</p>
+            <h5 class="cnt">EDUCATION</h5>
+            <ul>bachelor of engineering</ul>
+            <h5 CLASS="cnt">TECHNICAL SKILLS</h5>
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>figma</li>
+                <li>github</li>
+                <li>flutter</li>
+            </ul>
+            <h5 class="cnt">CERTIFICATION</h5>
+            <ul>
+                <li>Full stack web development</li>
+                <li>Artificial intelligence</li>
+                <li>Internet of things</li>
+            </ul>
+            <h5 class="cnt">MINI PROJECT</h5>
+            <ul>
+                <li>food app design</li>
+                <li>coffee shop</li>
+            </ul>
+            <h5 class="cnt">EXTRACURRICULAR ACTIVITIES</h5>
+            <ul>
+                <li>cricket</li>
+                <li>basketball</li>
+                <li>football</li>
+            </ul>
+      
+    </div>
+  );
+}
+
+export default App;
